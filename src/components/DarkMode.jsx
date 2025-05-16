ae0040d1ef79ce23cbe2efab2a82ab2fb5546ce0
HooMaN <eng.homan@gmail.com>
2025-05-16 18:50:33 +0330

    feat: :sparkles: I implemented the Navbar component with dark mode and search bar
    I created the Navbar.jsx component with a responsive navigation menu, dropdown links, search bar, and cart icon. Added DarkMode.jsx for theme toggling with image-based buttons. Styled the search bar in index.css using Tailwind's @layer. Integrated the Navbar into App.jsx.

---
+++ src/components/DarkMode.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import LightButton from "../assets/website/light-mode-button.png";
+import DarkButton from "../assets/website/dark-mode-button.png";
+
+const DarkMode = () => {
+  const [theme, setTheme] = useState(localStorage.theme || "light");
+
+  useEffect(() => {
+    localStorage.theme = theme;
+    document.documentElement.classList.toggle("dark", theme === "dark");
+  }, [theme]);
+
+  return (
+    <div onClick={() => setTheme(theme === "dark" ? "light" : "dark")} className="relative">
+      <img
+        src={LightButton}
+        alt="LightButton"
+        className={`w-12 cursor-pointer absolute right-0 z-10 transition-all duration-300 ${theme === "dark" ? "opacity-0" : "opacity-100"}`}
+      />
+      <img src={DarkButton} alt="DarkButton" className="w-12 cursor-pointer" />
+    </div>
+  );
+};
+
+export default DarkMode;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,116 @@
+import { FaCartShopping } from "react-icons/fa6";
+import { IoMdSearch } from "react-icons/io";
+import DarkMode from "./DarkMode";
+import { FaCaretDown } from "react-icons/fa";
+
+const menuLinks = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Shop",
+    link: "/#shop",
+  },
+  {
+    id: 3,
+    name: "About",
+    link: "/#about",
+  },
+  {
+    id: 4,
+    name: "Blogs",
+    link: "/#blog",
+  },
+];
+
+const DropdownLinks = [
+  {
+    id: 1,
+    name: "Trending Products",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Best Selling",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Top Rated",
+    link: "/#",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40 ">
+      <div className="py-4">
+        <div className="container flex justify-between items-center">
+          {/* logo and links section */}
+          <div className="flex items-center gap-4">
+            <a href="#" className="text-primary font-semibold tracking-widest text-2xl uppercase sm:text-3xl">
+              Eshop
+            </a>
+            {/* menu items */}
+            <div className="hidden lg:block">
+              <ul className="flex items-center gap-4 ">
+                {menuLinks.map((data, index) => (
+                  <li key={index}>
+                    <a href={data.link} className="inline-block px-4 font-semibold text-gray-500 hover:text-black dark:hover:text-white duration-200">
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
+                {/* dropdown */}
+                <li className="relative cursor-pointer group">
+                  <a href="#" className="flex items-center gap-[2px] font-semibold text-gray-500 dark:hover:text-white py-2">
+                    Quick Links
+                    <span>
+                      <FaCaretDown className="group-hover:rotate-180 duration-300" />
+                    </span>
+                  </a>
+                  {/* dropdown items */}
+                  <div className="absolute z-50 hidden group-hover:block w-[200px] rounded-md bg-white shadow-md dark:bg-gray-900 p-2 text-black dark:text-white">
+                    <ul className="space-y-2">
+                      {DropdownLinks.map((data) => (
+                        <li key={data.id}>
+                          <a
+                            href={data.link}
+                            className="text-gray-500 dark:hover:text-white duration-200 p-2 w-full hover:bg-primary/20 inline-block rounded-md font-semibold">
+                            {data.name}
+                          </a>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+          {/* navbar right section */}
+          <div className="flex justify-between items-center gap-4">
+            {/* search bar */}
+            <div className="relative group hidden sm:block">
+              <input type="text" placeholder="Search" className="search-bar" />
+              <IoMdSearch className="text-xl text-gray-600 group-hover:text-primary dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3 duration-200" />
+            </div>
+            {/* order-button secion */}
+            <button className="relative p-3">
+              <FaCartShopping className="text-xl text-gray-600 dark:text-gray-400" />
+              <div className="w-4 h-4 bg-red-500 text-white rounded-full absolute top-0 right-0 flex items-center justify-center text-xs">4</div>
+            </button>
+            {/* dark mode */}
+            <div>
+              <DarkMode />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
