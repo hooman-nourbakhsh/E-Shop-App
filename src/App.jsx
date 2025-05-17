42617216306a969cb44fe8a36d3f3b8ea9d969ff
HooMaN <eng.homan@gmail.com>
2025-05-17 22:11:13 +0330

    feat: :sparkles: I added the Services component
    I created Services.jsx with a responsive grid displaying service features using react-icons, defined ServiceData for dynamic rendering, and integrated the Services component into App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Category from "./components/Category";
+import Services from "./components/Services";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden">
       <Navbar />
       <Hero />
       <Category />
+      <Services />
     </div>
   );
 };
